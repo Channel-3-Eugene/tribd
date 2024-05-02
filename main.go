9ee879b7ab99338364da363f47a25c181b42836e
Rob Martin <rob@version2beta.com>
2024-05-01 21:02:58 -0500

    add pacing with a token bucket algorithm

--- main.go
+++ main.go
@@ -7,12 +7,74 @@ import (
 	"syscall"
 )
 
+package main
+
+import (
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+	"time"
+)
+
+// TokenBucketController represents a token bucket controller.
+type TokenBucketController struct {
+	rate          float64 // Rate of token replenishment (tokens per second)
+	tokenBucket   chan struct{}
+	quitCh        chan struct{}
+}
+
+// NewTokenBucketController creates a new token bucket controller.
+func NewTokenBucketController(rate float64, bucketSize int) *TokenBucketController {
+	return &TokenBucketController{
+		rate:          rate,
+		tokenBucket:   make(chan struct{}, bucketSize),
+		quitCh:        make(chan struct{}),
+	}
+}
+
+// Start starts the token bucket controller.
+func (tbc *TokenBucketController) Start() {
+	go func() {
+		defer close(tbc.tokenBucket)
+		ticker := time.NewTicker(time.Second / time.Duration(tbc.rate))
+		defer ticker.Stop()
+		for {
+			select {
+			case <-ticker.C:
+				select {
+				case tbc.tokenBucket <- struct{}{}:
+				default:
+					// Token bucket full, discard token
+				}
+			case <-tbc.quitCh:
+				return
+			}
+		}
+	}()
+}
+
+// Stop stops the token bucket controller.
+func (tbc *TokenBucketController) Stop() {
+	close(tbc.quitCh)
+}
+
+// InputStream represents an input stream read from UDP.
+type InputStream struct {
+	// Fields omitted for brevity
+}
+
+// Start starts reading from the input stream and sending data to otwPacketCh.
+func (inputStream *InputStream) Start(otwPacketCh chan<- OTWPacket, stopCh <-chan struct{}) {
+	// Example implementation omitted for brevity
+}
+
 func main() {
-	// read and parse config
+	// Read and parse config
 	var c Config
 	closer, err := c.Read(...)
 	if err != nil {
-		// handle error
+		// Handle error
 	}
 
 	// Close the resources when main exits
@@ -28,17 +90,12 @@ func main() {
 	sigCh := make(chan os.Signal, 1)
 	signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM, syscall.SIGHUP)
 
-	// Create a slice to collect the channels for stopping readers
-	stopChannels := make([]chan struct{}, len(c.InputStreams))
-
-	// Start a goroutine to close channels when all waitgroup members are gone
-	go func() {
-		wg.Wait()
-		close(otwPacketCh)
-	}()
+	// Start a token bucket controller to control the rate of data transmission
+	tokenBucketController := NewTokenBucketController(100.0, 1000) // Example: 100 tokens per second, bucket size of 1000
+	tokenBucketController.Start()
+	defer tokenBucketController.Stop()
 
 	// Start a goroutine to handle OS signals
-	// Move to a function
 	go func() {
 		for {
 			select {
@@ -46,9 +103,7 @@ func main() {
 				switch sig {
 				case syscall.SIGINT, syscall.SIGTERM:
 					// Shutdown gracefully
-					for _, stopCh := range stopChannels {
-						close(stopCh)
-					}
+					closer()
 					return
 				case syscall.SIGHUP:
 					// Handle SIGHUP (reload config, etc.)
@@ -59,15 +114,12 @@ func main() {
 
 	// Start a goroutine for each reader config in c.InputStreams
 	for _, readerConfig := range c.InputStreams {
-		stopChannel := make(chan struct{}) // Create a stop channel for this reader
-		stopChannels = append(stopChannels, stopChannel)
-
 		wg.Add(1)
-		inputStream := NewInputStream(c.ReaderConfig)
-		go func(inputStream InputStream, stopCh <-chan struct{}) {
+		inputStream := NewInputStream(readerConfig)
+		go func(inputStream InputStream) {
 			defer wg.Done()
-			inputStream.Start(otwPacketCh, stopCh)
-		}(readerConfig, stopChannel)
+			inputStream.Start(otwPacketCh, tokenBucketController.tokenBucket)
+		}(inputStream)
 	}
 
 	// Start a new writer from the config
@@ -77,9 +129,11 @@ func main() {
 	for {
 		select {
 		case packet := <-otwPacketCh:
+			// Wait until a token is available in the token bucket
+			<-tokenBucketController.tokenBucket
 			// Write the packet to a file
 			if err := writer.Write(packet); err != nil {
-				// handle error
+				// Handle error
 			}
 		}
 	}

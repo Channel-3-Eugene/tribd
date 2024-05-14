95eff40cd8133de0adf7db52b1813f7ab1027e6a
Rob Martin <rob@version2beta.com>
2024-05-14 15:33:08 -0500

    fix race conditions

--- channels/channels.go
+++ channels/channels.go
@@ -9,11 +9,15 @@ import (
 type Packet struct {
 	buffer []byte
 	pool   *sync.Pool
+	mu     sync.Mutex // Protects buffer and pool fields
 }
 
 // Data retrieves the data and automatically releases the buffer back to the pool.
 // It returns a copy of the data to ensure safety after buffer release.
 func (p *Packet) Data() []byte {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+
 	data := make([]byte, len(p.buffer))
 	copy(data, p.buffer)
 	p.release() // Manually release the buffer to the pool
@@ -29,8 +33,8 @@ func (p *Packet) release() {
 type PacketChan struct {
 	ch     chan *Packet
 	pool   *sync.Pool
-	mu     sync.RWMutex // Protects closing of channel
-	closed bool         // Indicates if the channel is closed
+	mu     sync.Mutex // Protects closing of channel and sending
+	closed bool       // Indicates if the channel is closed
 }
 
 // NewPacketChan creates a new PacketChan with the specified buffer size.
@@ -50,19 +54,16 @@ func NewPacketChan(size int) *PacketChan {
 }
 
 // Send sends a packet to the channel and handles channel closure gracefully.
-func (p *PacketChan) Send(data []byte) (err error) {
-	defer func() {
-		if r := recover(); r != nil {
-			err = fmt.Errorf("send on closed channel")
-		}
-	}()
-
+func (p *PacketChan) Send(data []byte) error {
 	packet := p.pool.Get().(*Packet)
-	packet.pool = p.pool                               // Assign the pool reference here
-	packet.buffer = append(packet.buffer[:0], data...) // Reuse buffer, resetting and copying data
+	packet.pool = p.pool // Assign the pool reference here
 
-	p.mu.RLock()
-	defer p.mu.RUnlock()
+	packet.mu.Lock()
+	packet.buffer = append(packet.buffer[:0], data...) // Reuse buffer, resetting and copying data
+	packet.mu.Unlock()
+
+	p.mu.Lock()
+	defer p.mu.Unlock()
 	if p.closed {
 		packet.release() // Ensure buffer is released if channel is closed
 		return fmt.Errorf("failed to send data: channel closed")
@@ -83,8 +84,7 @@ func (p *PacketChan) Receive() []byte {
 	if !ok {
 		return nil // Channel closed
 	}
-	data := packet.Data() // This handles the release of the buffer
-	return data
+	return packet.Data() // This handles the release of the buffer
 }
 
 // Close closes the channel to prevent further sends.
